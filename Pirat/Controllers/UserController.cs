a5fadcdd71617d0434574b3e5307affe5b82bcc3
Max Wiesholler <m.wiesholler@campus.lmu.de>
2020-03-21 00:54:13 +0100

    Basic template for backend project

---
+++ Pirat/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pirat.Controllers
+{
+    [ApiController]
+    [Route("/user")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [Produces("application/json")]
+        public IActionResult Get()
+        {
+            var user = new User();
+            user.Age = 20;
+            user.Name = "Dummy";
+            return Ok(user);
+        }
+    }
+
+}
---
+++ Pirat/Model/User.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pirat
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
