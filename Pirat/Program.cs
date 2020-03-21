d1cb9af3000323131d7a8808ce9e366c200e41ba
Max Wiesholler <m.wiesholler@campus.lmu.de>
2020-03-21 16:31:43 +0100

    Always run on 5000 with http

--- Pirat/Program.cs
+++ Pirat/Program.cs
@@ -21,6 +21,7 @@ namespace Pirat
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:5000");
                 });
     }
 }
