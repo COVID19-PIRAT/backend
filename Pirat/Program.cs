cc1940f1e7c283d24ad8e6b87e500153b6fbc15f
Chaoran Chen <mail@chaoran-chen.de>
2020-03-28 01:01:16 +0100

    Add check of environment variables at startup
    Closes #20

--- Pirat/Program.cs
+++ Pirat/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -13,8 +14,35 @@ namespace Pirat
     {
         public static void Main(string[] args)
         {
+            CheckEnvironmentVariables();
             CreateHostBuilder(args).Build().Run();
         }
+
+
+        public static void CheckEnvironmentVariables()
+        {
+            List<string> requiredEnvironmentVariables = new List<string>()
+            {
+                "PIRAT_SENDER_MAIL_ADDRESS",
+                "PIRAT_SENDER_MAIL_PASSWORD",
+                "PIRAT_SENDER_MAIL_USERNAME",
+                "PIRAT_SENDER_MAIL_SMTP_HOST",
+                "PIRAT_INTERNAL_RECEIVER_MAIL",
+
+                "PIRAT_HOST",
+                "PIRAT_CONNECTION",
+                "PIRAT_GOOGLE_API_KEY",
+                "PIRAT_GOOGLE_RECAPTCHA_SECRET"
+            };
+            foreach (var requiredEnvironmentVariable in requiredEnvironmentVariables)
+            {
+                if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable(requiredEnvironmentVariable)))
+                {
+                    throw new Exception("Environment variable is missing: " + requiredEnvironmentVariable);
+                }
+            }
+        }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
