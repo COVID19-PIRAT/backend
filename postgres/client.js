6af9a8a1cce9c5a6c71fa418f6f78a8ae61cfe8c
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2020-03-29 18:47:22 +0200

    Fix tables and add data for the tests

--- postgres/client.js
+++ postgres/client.js
@@ -32,7 +32,32 @@ const addressTable = `create table address
 alter table address owner to postgres;
 
 create unique index address_id_uindex
-	on address (id);`
+	on address (id);
+`
+
+const offerTable = `create table offer
+(
+	id serial not null
+		constraint offer_pkey
+			primary key,
+	name text not null,
+	mail text not null,
+	phone text,
+	organisation text not null,
+	ispublic boolean default false not null,
+	address_id integer
+		constraint provider_address_id_fk
+			references address
+				on update cascade on delete cascade,
+	consumable_ids integer[],
+	device_ids integer[],
+	personal_ids integer[],
+	token text not null,
+	timestamp timestamp not null
+);
+
+alter table offer owner to postgres;
+`
 
 const consumableTable = `create table consumable
 (
@@ -87,30 +112,6 @@ create unique index device_id_uindex
 	on device (id);
 `
 
-const offerTable = `create table offer
-(
-	id serial not null
-		constraint offer_pkey
-			primary key,
-	name text not null,
-	mail text not null,
-	phone text,
-	organisation text not null,
-	ispublic boolean default false not null,
-	address_id integer
-		constraint provider_address_id_fk
-			references address
-				on update cascade on delete cascade,
-	consumable_ids integer[],
-	device_ids integer[],
-	personal_ids integer[],
-	token text not null,
-	timestamp timestamp not null
-);
-
-alter table offer owner to postgres;
-`
-
 const personalTable = `create table personal
 (
 	id serial not null
@@ -135,32 +136,65 @@ alter table personal owner to postgres;
 
 create unique index manpower_id_uindex
 	on personal (id);
-
 `
 
-//Check tables
-pgclient.query(addressTable, (err, res) => {
-    if (err) throw err
-});
-pgclient.query(consumableTable, (err, res) => {
-    if (err) throw err
-});
-pgclient.query(deviceTable, (err, res) => {
-    if (err) throw err
-});
-pgclient.query(offerTable, (err, res) => {
-    if (err) throw err
-});
-pgclient.query(personalTable, (err, res) => {
-    if (err) throw err
-});
+//Create the tables
 
-// pgclient.query(text, values, (err, res) => {
-//     if (err) throw err
-// });
+var tables = [addressTable, consumableTable, deviceTable, offerTable, personalTable]
 
-// pgclient.query('SELECT * FROM student', (err, res) => {
-//     if (err) throw err
-//     console.log(err, res.rows) // Print the data in student table
-//     pgclient.end()
-// });
+for(i = 0; i < tables.length; i++){
+    pgclient.query(tables[i], (err, res) => {
+        if (err) throw err
+    });
+}
+
+const addressProvider = `INSERT INTO address 
+(id, streetnumber, postalcode, city, country, hascoordinates, latitude, longitude, street) 
+VALUES (1, '77', '27498', 'Helgoland', 'Deutschland', true, 54.1830721, 7.8863887, 'Hauptstraße');
+`
+
+const offer = `INSERT INTO offer 
+(id, name, mail, phone, organisation, ispublic, address_id, consumable_ids, device_ids, personal_ids, token, timestamp) 
+VALUES (37, 'Störtebeker', 'pirat.hilfsmittel@gmail.com', '987654', 'Instiut für Piraterie', true, 1, '{1}', '{1}', '{1}', 'FAUWc7MO4MM5M1upNhxUkZ9aArdlVo', 
+'2020-03-29 17:58:12.041110');
+`
+
+const addressConsumable = `INSERT INTO address 
+(id, streetnumber, postalcode, city, country, hascoordinates, latitude, longitude, street) 
+VALUES (2, '77', '27498', 'Helgoland', 'Deutschland', true, 54.1830721, 7.8863887, 'Hauptstraße');
+`
+
+const consumable = `INSERT INTO consumable 
+(id, category, name, manufacturer, ordernumber, amount, offer_id, address_id, unit, annotation) 
+VALUES (1, 'Rum', 'Nordrum', 'Störtebeker & Co', '999', 100, 1, 2, 'Liter', 'Arrr');
+`
+
+const addressPersonal = `INSERT INTO public 
+(id, streetnumber, postalcode, city, country, hascoordinates, latitude, longitude, street) 
+VALUES (3, '77', '27498', 'Helgoland', 'Deutschland', true, 54.1830721, 7.8863887, 'Hauptstraße');
+`
+
+const personal = `INSERT INTO personal 
+(id, qualification, institution, researchgroup, area, experience_rt_pcr, annotation, offer_id, address_id) 
+VALUES (1, 'Kapitän', 'Institut für Piraterie', 'Piraten Ahoi', 'Schiffsfahrt, Piraterie', false, 'Ahoi!', 1, 3);
+`
+
+const addressDevice = `INSERT INTO public.address 
+(id, streetnumber, postalcode, city, country, hascoordinates, latitude, longitude, street) 
+VALUES (4, '77', '27498', 'Helgoland', 'Deutschland', true, 54.1830721, 7.8863887, 'Hauptstraße');
+`
+
+const device = `INSERT INTO device 
+(category, name, id, manufacturer, ordernumber, amount, offer_id, address_id, annotation) 
+VALUES ('Schiffsmaterial', 'Steuerrad', 1, 'Störtebeker & Co', '12345', 10, 1, 4, 'Volle Fahrt voraus!');
+`
+
+//Add data
+
+var insertions = [addressProvider, offer, addressConsumable, consumable, addressPersonal, personal, addressDevice, device]
+
+for(i = 0; i < insertions.length; i++){
+    pgclient.query(insertions[i], (err, res) => {
+        if (err) throw err
+    });
+}
