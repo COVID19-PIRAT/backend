a53ec499c9fcac4b0fa0768ac42cf44a61b0becd
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2020-03-29 22:23:33 +0200

    Tests Directories and fxcop for code analysis

---
+++ Pirat.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Pirat.IntegrationTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
