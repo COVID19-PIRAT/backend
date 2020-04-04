d786c44452fb06f9dc8dd119bc437049d144c2eb
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2020-04-04 19:28:10 +0200

    More tests for TDD

--- Pirat.DatabaseTests/ResourceChangeTest.cs
+++ Pirat.DatabaseTests/ResourceChangeTest.cs
@@ -15,7 +15,7 @@ using Xunit;
 
 namespace Pirat.DatabaseTests
 {
-    public class ResourceChangeTest
+    public class ResourceChangeTest : IDisposable
     {
         private const string connectionString =
             "Server=localhost;Port=5432;Database=postgres;User ID=postgres;Password=postgres";
@@ -52,6 +52,7 @@ namespace Pirat.DatabaseTests
             _captainHookGenerator = new CaptainHookGenerator();
             _shyPirateGenerator = new ShyPirateGenerator();
 
+
             var offer = _captainHookGenerator.generateOffer();
             _token =  _resourceUpdateService.insert(offer).Result;
         }
@@ -72,7 +73,7 @@ namespace Pirat.DatabaseTests
         }
 
         [Fact(Skip = "TODO")]
-        public void Test_ChangeProviderInformation()
+        public void Test_ChangeProviderInformation_Possible()
         {
             var provider = _captainHookGenerator.GenerateProvider();
             provider.name = "Peter Pan";
@@ -95,19 +96,224 @@ namespace Pirat.DatabaseTests
         }
 
         [Fact(Skip = "TODO")]
-        public void Test_ChangeConsumableInformation()
+        public void Test_ChangeProviderMail_NotPossible()
+        {
+            var provider = _captainHookGenerator.GenerateProvider();
+            var providerMailOriginal = provider.mail;
+            var providerMailChanged = "mail.changed@gmx.de";
+
+            provider.mail = providerMailChanged;
+
+            var exception = Record.Exception(() => _resourceUpdateService.ChangeInformation(_token, provider).Result);
+            Assert.Null(exception);
+
+            var device = _captainHookGenerator.GenerateDevice();
+            var response = _resourceDemandService.QueryOffers(device).Result;
+            Assert.NotNull(response);
+            Assert.NotEmpty(response);
+            var providerFromDevice = response.First().provider;
+            Console.Out.WriteLine(providerFromDevice);
+            Console.Out.WriteLine(provider);
+            Assert.True(providerMailOriginal.Equals(providerFromDevice.mail, StringComparison.Ordinal));
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangeProviderInformation_BadInputs()
+        {
+            var provider = _captainHookGenerator.GenerateProvider();
+            provider.name = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, provider).Result);
+
+            provider = _captainHookGenerator.GenerateProvider();
+            provider.organisation = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, provider).Result);
+
+            //provider = _captainHookGenerator.GenerateProvider();
+            //provider.phone = "";
+            //Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, provider).Result);
+
+            provider = _captainHookGenerator.GenerateProvider();
+            provider.address.postalcode = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, provider).Result);
+
+            provider = _captainHookGenerator.GenerateProvider();
+            provider.address.country = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, provider).Result);
+
+            provider = _captainHookGenerator.GenerateProvider();
+            provider.mail = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, provider).Result);
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangeConsumableInformation_Possible()
+        {
+            var consumable = _captainHookGenerator.GenerateConsumable();
+            consumable.name = "New name";
+            consumable.unit = "Kilogramm";
+            consumable.annotation = "Geändert";
+            consumable.manufacturer = "Doch wer anders";
+            consumable.ordernumber = "8877766";
+            consumable.address.postalcode = "85521";
+            consumable.address.country = "Seeland";
+
+            var exception = Record.Exception(() => _resourceUpdateService.ChangeInformation(_token, consumable).Result);
+            Assert.Null(exception);
+
+            var queryConsumable = _captainHookGenerator.GenerateConsumable();
+            var response = _resourceDemandService.QueryOffers(queryConsumable).Result;
+            Assert.NotNull(response);
+            Assert.NotEmpty(response);
+            var consumableFromQuery = response.First().resource;
+            Console.Out.WriteLine(consumableFromQuery);
+            Console.Out.WriteLine(consumable);
+            Assert.True(consumableFromQuery.Equals(consumable));
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangeConsumableInformation_NotPossible()
+        {
+            var consumable = _captainHookGenerator.GenerateConsumable();
+            var categoryOriginal = consumable.category;
+            consumable.category = "Doch was anderes";
+            var idOriginal = consumable.id;
+            consumable.id = 999999;
+
+            var exception = Record.Exception(() => _resourceUpdateService.ChangeInformation(_token, consumable).Result);
+            Assert.Null(exception);
+
+            var queryConsumable = _captainHookGenerator.GenerateConsumable();
+            var response = _resourceDemandService.QueryOffers(queryConsumable).Result;
+            Assert.NotNull(response);
+            Assert.NotEmpty(response);
+
+            var consumableFromQuery = response.First().resource;
+            Assert.True(consumableFromQuery.category.Equals(categoryOriginal));
+            Assert.True(consumableFromQuery.id == idOriginal);
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangeConsumableInformation_BadInputs()
+        {
+            var consumable = _captainHookGenerator.GenerateConsumable();
+            consumable.name = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, consumable).Result);
+
+            consumable = _captainHookGenerator.GenerateConsumable();
+            consumable.unit = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, consumable).Result);
+
+            consumable = _captainHookGenerator.GenerateConsumable();
+            consumable.category = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, consumable).Result);
+
+            consumable = _captainHookGenerator.GenerateConsumable();
+            consumable.address.postalcode = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, consumable).Result);
+
+            consumable = _captainHookGenerator.GenerateConsumable();
+            consumable.address.country = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, consumable).Result);
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangeDeviceInformation_Possible()
+        {
+            var device = _captainHookGenerator.GenerateDevice();
+            device.name = "New name";
+            device.annotation = "Geändert";
+            device.manufacturer = "Doch wer anders";
+            device.ordernumber = "8877766";
+            device.address.postalcode = "85521";
+            device.address.country = "Seeland";
+
+            var exception = Record.Exception(() => _resourceUpdateService.ChangeInformation(_token, device).Result);
+            Assert.Null(exception);
+
+            var queryDevice = _captainHookGenerator.GenerateDevice();
+            var response = _resourceDemandService.QueryOffers(queryDevice).Result;
+            Assert.NotNull(response);
+            Assert.NotEmpty(response);
+            var consumableFromDevice = response.First().resource;
+            Console.Out.WriteLine(consumableFromDevice);
+            Console.Out.WriteLine(device);
+            Assert.True(consumableFromDevice.Equals(device));
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangeDeviceInformation_NotPossible()
+        {
+            var device = _captainHookGenerator.GenerateDevice();
+            var categoryOriginal = device.category;
+            device.category = "Doch was anderes";
+            var idOriginal = device.id;
+            device.id = 999999;
+
+            var exception = Record.Exception(() => _resourceUpdateService.ChangeInformation(_token, device).Result);
+            Assert.Null(exception);
+
+            var queryDevice = _captainHookGenerator.GenerateDevice();
+            var response = _resourceDemandService.QueryOffers(queryDevice).Result;
+            Assert.NotNull(response);
+            Assert.NotEmpty(response);
+
+            var deviceFromQuery = response.First().resource;
+            Assert.True(deviceFromQuery.category.Equals(categoryOriginal));
+            Assert.True(deviceFromQuery.id == idOriginal);
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangeDeviceInformation_BadInputs()
+        {
+            var device = _captainHookGenerator.GenerateDevice();
+            device.name = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, device).Result);
+
+            device = _captainHookGenerator.GenerateDevice();
+            device.category = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, device).Result);
+
+            device = _captainHookGenerator.GenerateDevice();
+            device.address.postalcode = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, device).Result);
+
+            device = _captainHookGenerator.GenerateDevice();
+            device.address.country = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, device).Result);
+        }
+
+        [Fact(Skip = "TODO")]
+        public void Test_ChangePersonalInformation_Possible()
         {
 
         }
 
         [Fact(Skip = "TODO")]
-        public void Test_ChangeDeviceInformation()
+        public void Test_ChangePersonalInformation_BadInputs()
         {
+            var personal = _captainHookGenerator.GeneratePersonal();
+            personal.qualification = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, personal).Result);
 
+            personal = _captainHookGenerator.GeneratePersonal();
+            personal.institution = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, personal).Result);
+
+            personal = _captainHookGenerator.GeneratePersonal();
+            personal.area = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, personal).Result);
+
+            personal = _captainHookGenerator.GeneratePersonal();
+            personal.address.postalcode = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, personal).Result);
+
+            personal = _captainHookGenerator.GeneratePersonal();
+            personal.address.country = "";
+            Assert.Throws<ArgumentException>(() => _resourceUpdateService.ChangeInformation(_token, personal).Result);
         }
 
         [Fact(Skip = "TODO")]
-        public void Test_ChangePersonalInformation()
+        public void Test_ChangePersonalInformation_NotPossible()
         {
 
         }
