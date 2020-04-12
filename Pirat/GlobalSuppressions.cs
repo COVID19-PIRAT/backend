091992da157ae6b557723f2be7074ad4a9f3e8af
Maximilian Wiesholler <maximilian.wiesholler@tum.de>
2020-04-12 17:18:00 +0200

    Suppress warning to use resource tables

--- Pirat/GlobalSuppressions.cs
+++ Pirat/GlobalSuppressions.cs
@@ -7,3 +7,4 @@ using System.Diagnostics.CodeAnalysis;
 
 [assembly: SuppressMessage("Reliability", "CA2007:Consider calling ConfigureAwait on the awaited task", Justification = "This is an application context which should relie on default behavior https://devblogs.microsoft.com/dotnet/configureawait-faq/")]
 [assembly: SuppressMessage("Security", "SEC0121:CORS Allow Origin Wildcard", Justification = "So far we do not have any specific policies", Scope = "member", Target = "~M:Pirat.Startup.ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
+[assembly: SuppressMessage("Globalization", "CA1303:Literale nicht als lokalisierte Parameter übergeben", Justification = "Resource Tables for the FailureCodes would mean a lot of extra code")]
