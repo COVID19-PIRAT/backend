3df78a87398f4710d49e1e089647cabaaec58965
Timo Weike <timo_weike@gmx.de>
2020-04-13 18:08:07 +0200

    Added a ComputeDistance variant
    that only takes double values
    
    #Issue #29

--- Pirat/Services/Helper/DistanceCalculator.cs
+++ Pirat/Services/Helper/DistanceCalculator.cs
@@ -4,23 +4,33 @@ namespace Pirat.Services.Helper
 {
     public static class DistanceCalculator
     {
-        public static double computeDistance(decimal latitude1, decimal longitude1, decimal latitude2, decimal longitude2)
+        public static double ComputeDistance(double latitude1, double longitude1, double latitude2, double longitude2)
         {
             //made a short sketch on paper and just got the formula ;)
-            int earthRadius = 6371; //km
-            var latitudeRadian = ConvertDegreesToRadians(Decimal.ToDouble(latitude2 - latitude1));
-            var longitudeRadian = ConvertDegreesToRadians(decimal.ToDouble(longitude2 - longitude1));
-
+            var latitudeRadian = ConvertDegreesToRadians(latitude2 - latitude1);
+            var longitudeRadian = ConvertDegreesToRadians(longitude2 - longitude1);
 
             var a = Math.Sin(latitudeRadian / 2) * Math.Sin(latitudeRadian / 2) +
-                    Math.Cos(ConvertDegreesToRadians(Decimal.ToDouble(latitude1))) *
-                    Math.Cos(ConvertDegreesToRadians(Decimal.ToDouble(latitude2))) * Math.Sin(longitudeRadian / 2) *
+                    Math.Cos(ConvertDegreesToRadians(latitude1)) *
+                    Math.Cos(ConvertDegreesToRadians(latitude2)) * Math.Sin(longitudeRadian / 2) *
                     Math.Sin(longitudeRadian / 2);
 
             var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-            var d = earthRadius * c;
+            var d = Location.EarthRadius * c;
             return d;
         }
+
+        public static double ComputeDistance(
+            decimal latitude1,
+            decimal longitude1,
+            decimal latitude2,
+            decimal longitude2)
+            => ComputeDistance(
+                decimal.ToDouble(latitude1),
+                decimal.ToDouble(longitude1),
+                decimal.ToDouble(latitude2),
+                decimal.ToDouble(longitude2)
+            );
 
         private static double ConvertDegreesToRadians(double degrees)
         {
